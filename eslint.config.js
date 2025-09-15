7f01a6bd73fa192d8880080d5ba1da95f8f1755a
elaine <elainedb.dev@gmail.com>
2025-09-15 16:25:22 +0200

    v1 - add google sign in

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,11 @@ const expoConfig = require('eslint-config-expo/flat');
 module.exports = defineConfig([
   expoConfig,
   {
+    settings: {
+      'import/resolver': {
+        'babel-module': {},
+      },
+    },
     ignores: ['dist/*'],
   },
 ]);
